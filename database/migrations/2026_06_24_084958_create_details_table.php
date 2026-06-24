ae1ca90ef761170cd400b3de16dda09f583b9442
Alan GIRALT <alangiralt@telefonica.net>
2026-06-24 11:00:47 +0200

    feat: setup details model and migration with 9,4 decimal structure for large orders

---
+++ database/migrations/2026_06_24_084958_create_details_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('details', function (Blueprint $table) {
+            $table->foreignId('order_id')->constrained('orders'); 
+            $table->foreignId('child_product_id')->constrained('child_products'); 
+            $table->integer('discount'); 
+            $table->integer('quantity'); 
+            $table->decimal('sale_unit_price', 9, 4);
+            $table->primary(['order_id', 'child_product_id']);
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('details');
+    }
+};
