dea66b78ed36149da1ed980ffa687da3810af92c
Alan GIRALT <alangiralt@telefonica.net>
2026-06-26 09:32:07 +0200

    fix: add composite unique index to category and father_id in categories table

--- database/migrations/2026_06_24_073044_create_categories_table.php
+++ database/migrations/2026_06_24_073044_create_categories_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('category');
             $table->foreignId('father_id')->nullable()->constrained('categories');
+            $table->unique(['category', 'father_id']);
         });
     }
 
