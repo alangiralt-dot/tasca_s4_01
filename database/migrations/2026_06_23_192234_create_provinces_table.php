b42246a404e43500112af1ab623197e23ea5441d
Alan GIRALT <alangiralt@telefonica.net>
2026-06-26 07:56:36 +0200

    fix: add unique constraint to province column in provinces table

--- database/migrations/2026_06_23_192234_create_provinces_table.php
+++ database/migrations/2026_06_23_192234_create_provinces_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('provinces', function (Blueprint $table) {
             $table->id();
-            $table->string('province');
+            $table->string('province')->unique();
         });
     }
 
